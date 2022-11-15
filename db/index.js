6c675c635f2fd94e3d1aa2c00a815645625a6e9e
NHNaims-code <mdnimurhasan99@gmail.com>
2022-11-15 15:40:43 +0600

    mongo string fixed

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-    .connect(" mongodb+srv://developernaim20:developernaim20@cluster0.sjmet.mongodb.net/demo_lawnmower2?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect("mongodb+srv://developernaim20:developernaim20@cluster0.sjmet.mongodb.net/demo_lawnmower2?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to mongodb..."))
     .catch(e => {
         console.error('Connection error', e.message);
